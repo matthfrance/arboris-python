d9b64ddd65714668caaa05acca914a2e39270a39
Yoann Sababady <ysababady@ysababady.(none)>
2010-01-10 19:03:46 +0100

    TestSuite_second_part

--- tests/TestSuite.py
+++ tests/TestSuite.py
@@ -5,8 +5,10 @@ import unittest
 import ConstraintsTests
 import JointsTests
 import UpdateDynamicTests
+import homogeneousmatrixTest
+import Human36Tests
 
 tests = unittest.TestSuite([ConstraintsTests.ts, JointsTests.ts,
-                            UpdateDynamicTests.ts])
+                            UpdateDynamicTests.ts, homogeneousmatrixTest.ts, Human36Tests.ts])
 
 unittest.TextTestRunner(verbosity=2).run(tests)
